4a9f6488f55761e800b3ddc3aabe65fc50465268
Aman Jain <128373401+AmanJain011@users.noreply.github.com>
2025-01-10 10:06:20 +0530

    Handling input validations and adding custom functions `stringLength` stringCopy`

--- arrayOfNames.c
+++ arrayOfNames.c
@@ -1,59 +1,74 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
 #define MAX_NAME_LENGTH 50
 #define MAX_ROWS 10
 #define MAX_COLUMNS 101
 
-int startsWithVowel(char name[MAX_NAME_LENGTH + 1])
-{
-    char firstChar = tolower(name[0]);
-    return (firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u');
+int startsWithVowel(char name[MAX_NAME_LENGTH + 1]){
+    char firstChar = name[0];
+    return (firstChar == 'A' || firstChar == 'E' || firstChar == 'I' || firstChar == 'O' || firstChar == 'U' || firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u');
 }
 
-int main()
-{
-    int rowCount, colCount;
+void clearBuffer(){
+    while (getchar() != '\n');
+}
+
+int getIntegerInput(char *prompt, int minValue, int maxValue){
+    int value;
+    printf("%s", prompt);
+    while (scanf("%d", &value) != 1 || value < minValue || value > maxValue){
+        printf("Invalid Input.\n");
+        printf("%s", prompt);
+        clearBuffer();
+    }
+    clearBuffer();
+    return value;
+}
+
+int stringLength(char *string){
+    int count = 0;
+    while(string[count++] != '\0');
+    return count;
+}
+
+void stringCopy(char *destination, char *source){
+    int count = 0;
+    while (source[count] != '\0') {
+        destination[count] = source[count];
+        count++;
+    }
+    destination[count] = '\0';
+}
+
+int main(){
     int vowelCount = 0;
     char longestName[MAX_NAME_LENGTH + 1];
     int maxLength = 0;
 
-    printf("Enter number of rows: ");
-    if(scanf("%d", &rowCount) != 1){
-      return 1;
-    }
-    printf("Enter number of columns: ");
-    if(scanf("%d", &colCount) != 1){
-      return 1;
-    }
-
-    if (rowCount < 1 || rowCount > MAX_ROWS || colCount < 1 || colCount > MAX_COLUMNS)
-    {
-        printf("Invalid input for rows or columns.\n");
-        return 1;
-    }
+    int rowCount = getIntegerInput("Enter number of rows: ", 1, MAX_ROWS);
+    int columnCount = getIntegerInput("Enter number of rows: ", 1, MAX_COLUMNS);
 
     char names[MAX_ROWS][MAX_COLUMNS][MAX_NAME_LENGTH + 1];
 
     printf("Enter the names:\n");
     for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
     {
-        for (int colIndex = 0; colIndex < colCount; colIndex++)
+        for (int colIndex = 0; colIndex < columnCount; colIndex++)
         {
             printf("Name at (%d,%d): ", rowIndex, colIndex);
-            scanf("%s", names[rowIndex][colIndex]);
-
+            scanf("%50s", names[rowIndex][colIndex]);
+            clearBuffer();
+            
             if (startsWithVowel(names[rowIndex][colIndex]))
             {
                 vowelCount++;
             }
 
-            int nameLength = strlen(names[rowIndex][colIndex]);
+            int nameLength = stringLength(names[rowIndex][colIndex]);
             if (nameLength > maxLength)
             {
                 maxLength = nameLength;
-                strcpy(longestName, names[rowIndex][colIndex]);
+                stringCopy(longestName, names[rowIndex][colIndex]);
             }
         }
     }
@@ -61,7 +76,7 @@ int main()
     printf("\nThe 2D array of names is:\n");
     for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
     {
-        for (int colIndex = 0; colIndex < colCount; colIndex++)
+        for (int colIndex = 0; colIndex < columnCount; colIndex++)
         {
             printf("%s ", names[rowIndex][colIndex]);
         }

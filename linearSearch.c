8a8536684642b7508c0002a70e3a7385949602ff
Aman Jain <ajinhindauncity@gmail.com>
2025-01-28 00:16:08 +0530

    linear Search Added

---
+++ linearSearch.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int linearSearch(const int array[], int arraySize, int target) {
+    for (int index = 0; index < arraySize; index++) {
+        if (array[index] == target) {
+            return index;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int *array = (int *)malloc(sizeof(int) * size);
+    if (array == NULL) {
+        printf("Memory allocation failed.\n");
+        return 0;
+    }
+
+    printf("Enter elements in the array:\n");
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    int target;
+    printf("Enter the number to search: ");
+    scanf("%d", &target);
+
+    int resultIndex = linearSearch(array, size, target);
+    if (resultIndex != -1) {
+        printf("Linear Search: Element found at index %d\n", resultIndex);
+    } else {
+        printf("Linear Search: Element not found\n");
+    }
+
+    free(array);
+    return 0;
+}
